24fe57f49b6bda54ff1adc70840f159e90512d64
Thomas Khalil <thomas@khalilt.com>
2021-03-20 10:29:08 +0100

    initial commit

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# site
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/bitshifta/pen/yLJdWLa](https://codepen.io/bitshifta/pen/yLJdWLa).
+
+
---
+++ src/script.js
@@ -0,0 +1,31 @@
+(function () {
+  const list = [
+    "Cloud Computing",
+    // "TypeScript",
+    // "AWS",
+    // "NodeJS",
+    "Functional Programming",
+    // "Linux",
+    // "Javascript",
+    // "Containers",
+    // "Automation",
+    // "Bash",
+    // "CDK",
+    // "Java",
+    // "Clojure",
+    // "CI",
+    "Ruby On Rails"
+    // "Postgresql",
+    // "DynamoDB",
+    // "S3",
+    // "Kubernetes",
+    // "Ansible",
+    // "Chef",
+    // "Cassandra"
+  ];
+  setInterval(function () {
+    const target = document.getElementById("xxx");
+    const random = Math.floor(Math.random() * list.length);
+    target.innerText = list[random];
+  }, 1500);
+})();
